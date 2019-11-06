a25fc0dcd5ae9a29c58e853f57fb2020eaf108bf
hrca <hrca@crossdevtech.hr>
2019-11-06 15:07:39 +0100

    Adds laravel/ui package and sets hardcoded api endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,49 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('api/v1')->get('config', function (Request $request) {
+
+    return response()->json([
+          "description" => "",
+          "device_groups" => [
+              [
+                "id" => 123,
+                "enabled" => 1,
+                "type" => "rfid_trigger",
+                "name" => "Large Forklifts",
+                "options" => [
+                    "trigger_duration_ms" => 5000,
+                    "time_between_trigger_seconds" => 86400
+                ],
+                "devices" => [
+                    "name" => "Blue forklift #1",
+                    "rfid" => "293012938209198321038"
+                ],[
+                    "name" => "Blue forklift #2",
+                    "rfid" => "102938283850391832938"
+                ]
+              ],
+              [
+                "id" => 124,
+                "enabled" => 1,
+                "type" => "rfid_trigger",
+                "name" => "Scissor Lifts",
+                "options" => [
+                    "trigger_duration_ms" => 9000,
+                    "time_between_trigger_seconds" => 86400
+                ],
+                "devices" => [
+                    "name" => "Lift #1",
+                    "rfid" => "873274789238498748378"
+                ],
+                [
+                    "name" => "Lift #35",
+                    "rfid" => "129388380298483747437"
+                ]
+              ]
+          ]
+    ]);
+
+});
