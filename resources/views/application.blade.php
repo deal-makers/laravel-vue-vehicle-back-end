acd9ab53f9661b4a76055673539434bc9c34854e
hrca <hrca@crossdevtech.hr>
2019-11-21 17:20:32 +0100

    Modifies application.blade cro>eng plus comment issue

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- <link rel="icon" href="<%= BASE_URL %>favicon.ico"> -->
+    {{-- <link rel="icon" href="<%= BASE_URL %>favicon.ico"> --}}
 
     <title>{{ ENV('APP_NAME') }}</title>
     <!-- Styles -->
@@ -18,7 +18,7 @@
   </head>
   <body>
     <noscript>
-      <strong>Ispričavamo se ali Europlast admin neradi bez JavaScript-a. Molimo uključite ga da bi ste nastavili.</strong>
+        <strong>We are sorry, but Olympus needs JavaScript to be enabled in order to run. Please turn on JavaScript to continue.</strong>
     </noscript>
     <div id="app">
     </div>
